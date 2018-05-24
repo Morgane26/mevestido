75edc2c687b6f4fbaa08339286b0f495cbcdc11c
elise serres <eliseserres@hotmail.fr>
2018-05-24 14:21:10 +0200

    view new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,20 @@ class ArticlesController < ApplicationController
   end
 
   def create
-
+    @article = Article.new(meal_params)
+    @article.user = current_user
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
+
+private
+
+   def meal_params
+      params.require(:article).permit(:season, :usage)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :articles
+  get  "artciles", to: "articles#index"
+
+  get  "articles/new", to: "articles#new"
+  post  "articles", to: "articles#create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
