1042999b4159f225111f38d60bd72f86a9523ef4
Morgane PERENZONI <mperenzoni@gmail.com>
2018-05-30 18:22:04 +0200

    new addiction

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   devise :omniauthable, omniauth_providers: [:facebook]
 
   has_many :articles, dependent: :destroy
